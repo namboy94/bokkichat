8f05d11c0aebc670eb41780361e9cbdc7b8f0402
Hermann Krumrey <hermann@krumreyh.com>
2018-12-13 10:23:05 +0100

    Remove scripts option

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ if __name__ == "__main__":
         url="https://gitlab.namibsun.net/namibsun/python/bokkichat",
         license="GNU GPL3",
         packages=find_packages(),
-        scripts=list(map(lambda x: os.path.join("bin", x), os.listdir("bin"))),
         install_requires=[
             "typing"
         ],
